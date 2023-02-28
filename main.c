f1bc4f2484b78f0b368760be5899c01aaa237755
izkelazz <a.riverag@udc.es>
2023-02-28 18:24:26 +0100

    P1 final

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ void led_green_init()
 {
   SIM->COPC = 0; // Deshabilitar WatchDog
   SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK; 
+  // SIM->SCGC5 |= (1 << 12) //Esto coloca el pin12 a 1. Ademas no sobreescribe el registro por eso se hace de esta manera
+  //Es equivalente a la puerta logica OR que en C es "|=" a nivel de bit (bitwise). La instruccion realiza un OR bit a bit con un 1 en la pos 12
+  //AND seria SIM->SCG5 &~ (1 << 12)
   PORTD->PCR[5] = PORT_PCR_MUX(1);//Funcionamiento como multiplexor. O rexistro pcr de cada un dos portos DPORT->PCR[numPIN]
   GPIOD->PDDR |= (1 << 5);
   GPIOD->PSOR = (1 << 5);
@@ -30,29 +33,31 @@ void led_red_init()
   GPIOE->PSOR = (1 << 29);
 }
 
-
-void sw1_check(int count)
+void config_sw1()
 {
-  if ((GPIOC->PDIR & (1 << 3)) == 0){
-    delay();
-    if ((GPIOC->PDIR & (1<<3))==0){
-      count++;
-      while ((GPIOC->PDIR & (1 << 3)) == 0); 
-    }
-  }
-  return count;
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+  PORTC->PCR[3] = PORT_PCR_MUX(1);
+  GPIOC->PDDR |= (1 << 3);//Rexistro comun para todo o porto
+  GPIOC->PSOR = (1 << 3);
 }
 
-void sw2_check(int count)
+void config_sw2()
 {
-  if ((GPIOC->PDIR & (1 << 12)) == 0){
-    delay();
-    if ((GPIOC->PDIR & (1<<12))==0){
-      count++;
-      while ((GPIOC->PDIR & (1 << 12)) == 0); 
-    }
-  }
-  return count;
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+  PORTC->PCR[12] = PORT_PCR_MUX(1);
+  GPIOC->PDDR |= (1 << 12);//Rexistro comun para todo o porto
+  GPIOC->PSOR = (1 << 12);
+}
+
+
+int sw1_check(){
+  return (GPIOC->PDIR & ~(1 << 3));
+}
+
+int sw2_check(){
+  return (GPIOC->PDIR & ~(1 << 12));
 }
 
 // LED_RED = PTE29
@@ -69,27 +74,29 @@ void led_green_toggle(void)
 int main(void)
 {
   int sw1_count = 0;
-
+  int sw2_count = 0;
   // Configure input switches
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK; // Enable pull-up resistor
-  PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK; // Enable pull-up resistor
-  
+  config_sw1();
+  config_sw2();
+    
   led_green_init();
   led_red_init();
   
 
   while (1) {
-    sw1_count = sw1_check(sw1_count)
-    if ((sw1_count % 2) == 0){
-      GPIOE->PSOR = (1 << 29);
+    if (sw1_check()){
+      sw1_count = (sw1_count + 1) % 2;
     }
-    if ((sw1_count % 2) == 1){
-      GPIOE->PTOR = (1 << 29)
+    if (sw2_check()){
+      sw2_count = (sw2_count + 1) % 2;
+    }
+    if (sw1_count || sw2_count){
+      led_red_toggle();
+    } else {
+      led_green_toggle();
     }
     delay();
   }
 
   return 0;
 }
-
